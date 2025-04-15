3b870426c779aae5a6af8674b262d060fae75d21
Jiwook00 <jwkim775@gmail.com>
2025-04-15 20:49:00 +0900

    상품 상세 조회 기능 추가

--- src/main/kotlin/hello/itemservice/web/basic/BasicItemController.kt
+++ src/main/kotlin/hello/itemservice/web/basic/BasicItemController.kt
@@ -6,6 +6,7 @@ import jakarta.annotation.PostConstruct
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
@@ -17,6 +18,13 @@ class BasicItemController (private val itemRepository: ItemRepository) {
         val items = itemRepository.findAll()
         model.addAttribute("items", items)
         return "basic/items"
+    }
+
+    @GetMapping("/{itemId}")
+    fun item(@PathVariable itemId: Long, model: Model): String {
+        val item = itemRepository.findById(itemId)
+        model.addAttribute("item", item)
+        return "basic/item"
     }
 
     /**
