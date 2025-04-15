78972098e696f43e54117a184a5fbfc2c081f928
Jiwook00 <jwkim775@gmail.com>
2025-04-15 20:42:37 +0900

    상품 목록 조회 기능 추가

--- src/main/kotlin/hello/itemservice/web/basic/BasicItemController.kt
+++ src/main/kotlin/hello/itemservice/web/basic/BasicItemController.kt
@@ -1,4 +1,30 @@
 package hello.itemservice.web.basic
 
-class BasicItemController {
+import hello.itemservice.domain.item.Item
+import hello.itemservice.domain.item.ItemRepository
+import jakarta.annotation.PostConstruct
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping("/basic/items")
+class BasicItemController (private val itemRepository: ItemRepository) {
+
+    @GetMapping
+    fun items(model: Model): String {
+        val items = itemRepository.findAll()
+        model.addAttribute("items", items)
+        return "basic/items"
+    }
+
+    /**
+     * 테스트용 데이터 추가
+     */
+    @PostConstruct
+    fun init() {
+        itemRepository.save(Item(itemName = "testA", price = 10000, quantity = 20))
+        itemRepository.save(Item(itemName = "testB", price = 17000, quantity = 17))
+    }
 }
